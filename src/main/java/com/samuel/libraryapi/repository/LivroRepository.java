0882a03bd4fb17d06e2167716263a3bdf7b42070
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-03-20 17:03:52 -0300

    atualização de estudo

--- src/main/java/com/samuel/libraryapi/repository/LivroRepository.java
+++ src/main/java/com/samuel/libraryapi/repository/LivroRepository.java
@@ -1,10 +1,9 @@
 package com.samuel.libraryapi.repository;
-
 import com.samuel.libraryapi.model.Autor;
 import com.samuel.libraryapi.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
@@ -25,6 +24,10 @@ public interface LivroRepository extends JpaRepository<Livro, UUID> {
     //select * from livro where titulo = ? and preco = ?
     List<Livro> findByTituloAndPreco(String titulo, BigDecimal preco);
 
-    //select * from livro where titulo = ? or isbn = ?
-    List<Livro> findByTituloOrIsbn(String titulo, String isbn);
+    List<Livro> findByTituloOrIsbnOrderByTitulo(String titulo, String isbn);
+
+    //select * from livro where data_publicacao between ? and ?
+    List<Livro> findByDataPublicacaoBetween(LocalDate inicio, LocalDate fim);
+
+
 }
