0a9ba741ca64223b3b17abbe8157f49d7f8240e3
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-03-28 14:36:43 -0300

    Implementação da classe Servlet para estudo

--- src/main/java/com/samuel/libraryapi/controller/AutorController.java
+++ src/main/java/com/samuel/libraryapi/controller/AutorController.java
@@ -1,15 +1,16 @@
 package com.samuel.libraryapi.controller;
 
 import com.samuel.libraryapi.controller.dto.AutorDTO;
-import com.samuel.libraryapi.model.Autor;
 import com.samuel.libraryapi.service.AutorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/autores")
@@ -24,10 +25,17 @@ public class AutorController {
     }
 
     @PostMapping
-    public ResponseEntity salvar(@RequestBody AutorDTO autor) {
+    public ResponseEntity<Void> salvar(@RequestBody AutorDTO autor) {
         var autorEntidade = autor.mapearParaAutor();
         service.salvar(autorEntidade);
-        return new ResponseEntity("Autor Salvo com sucesso!" + autor, HttpStatus.CREATED); //cód 201
+
+        //http://localhost:8080/autores/2121212-dsds-23ds2-ds
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{id}")
+                .buildAndExpand(autorEntidade.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
 }
