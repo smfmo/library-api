71ae6c4e57078781f2deb8db2ba97e9c12721514
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-04-02 16:52:04 -0300

    atualização de estudo( anotação @Size)

--- src/main/java/com/samuel/libraryapi/controller/dto/AutorDto.java
+++ src/main/java/com/samuel/libraryapi/controller/dto/AutorDto.java
@@ -2,16 +2,19 @@ package com.samuel.libraryapi.controller.dto;
 import com.samuel.libraryapi.model.Autor;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
 public record AutorDto(UUID id,
                        @NotBlank(message = "campo Obrigátorio")
-                       String nome,
+                               @Size( min = 2 ,max = 100, message = "campo fora do tamanho padrão")
+                       String nome, //parei aqui ()
                        @NotNull(message = "campo obrigátorio")
                        LocalDate dataNascimento,
                        @NotBlank(message = "campo obrigatorio")
+                               @Size(max = 50, min = 2, message = "campo fora do tamanho padrão")
                        String nacionalidade) {
 
 
