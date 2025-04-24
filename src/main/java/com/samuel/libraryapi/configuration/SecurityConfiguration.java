24c22ca2c5999c35f3195237c7af5f431635602e
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-04-24 09:30:15 -0300

    Atualização de estudo: Implementação do Spring Security na API

---
+++ src/main/java/com/samuel/libraryapi/configuration/SecurityConfiguration.java
@@ -0,0 +1,26 @@
+package com.samuel.libraryapi.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable) //desativa CSRF (importantes em APIs Rest Já que não usam cookies)
+                .formLogin(Customizer.withDefaults()) // Habilita o formulário de login padrão do Spring Security
+                .httpBasic(Customizer.withDefaults()) // habilita autenticação básica via cabeçalho (Authorization)
+                .authorizeHttpRequests(authorizeRequests -> {
+                    authorizeRequests.anyRequest().authenticated(); //todas as requisições precisam estar autenticadas
+                })
+                .build();
+    }
+}
