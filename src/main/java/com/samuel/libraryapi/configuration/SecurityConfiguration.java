a0aca569c0f4edcb72ec626528c047f98d8cc576
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-04-25 11:03:06 -0300

    implementação do UserDetailsService e e criptografia da senha

--- src/main/java/com/samuel/libraryapi/configuration/SecurityConfiguration.java
+++ src/main/java/com/samuel/libraryapi/configuration/SecurityConfiguration.java
@@ -6,6 +6,12 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,16 +19,37 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfiguration {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
+    public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
+        return security
                 .csrf(AbstractHttpConfigurer::disable) //desativa CSRF (importantes em APIs Rest Já que não usam cookies)
+                .authorizeHttpRequests(authorizeRequests -> {
+                    authorizeRequests.anyRequest().authenticated(); //todas as requisições precisam estar autenticadas
+                })
                 .httpBasic(Customizer.withDefaults()) // habilita autenticação básica via cabeçalho (Authorization)
                 .formLogin(configurer -> {
                     configurer.loginPage("/login").permitAll();
                 })
-                .authorizeHttpRequests(authorizeRequests -> {
-                    authorizeRequests.anyRequest().authenticated(); //todas as requisições precisam estar autenticadas
-                })
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        UserDetails user1 = User.builder().
+                username("userTest")
+                .password(encoder.encode("1234"))
+                .roles("USER")
+                .build();
+        UserDetails user2 = User.builder().
+                username("admin")
+                .password(encoder.encode("4321"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
     }
 }
