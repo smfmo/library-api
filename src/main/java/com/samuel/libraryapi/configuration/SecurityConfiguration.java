392863e75c8d6d38f01125280da473623a248494
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-06-22 14:35:15 -0300

    implementação da dependência do Swagger, e customização no security para ignorar as urls do swagger

--- src/main/java/com/samuel/libraryapi/configuration/SecurityConfiguration.java
+++ src/main/java/com/samuel/libraryapi/configuration/SecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
@@ -50,6 +51,18 @@ public class SecurityConfiguration {
                 .addFilterAfter(jwtCustomAuthenticationFilter, BearerTokenAuthenticationFilter.class)
                 .build();
     }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers(
+                  "/v2/api-docs/**",
+                  "/v3/api-docs/**",
+                  "/swagger-resources/**",
+                  "/swagger-ui.html",
+                  "/swagger-ui/**",
+                  "/webjars/**"
+        );
+    } // utilizado para ignorar urls (excluir do filtro do security)
 
     //configura o prefixo Role
     @Bean
